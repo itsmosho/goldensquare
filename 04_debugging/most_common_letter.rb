17be5166117891b96b8aa170c02838aa75dfdc42
Moses Osho <mosesosho@MASTUDEMB67Q6L4.lan>
2022-08-23 15:40:20 +0100

    Debuggin 2@

--- 04_debugging/most_common_letter.rb
+++ 04_debugging/most_common_letter.rb
@@ -1,6 +1,6 @@
 def get_most_common_letter(text)
     counter = Hash.new(0)
-    text.chars.each do |char|
+    text.split.join.chars.each do |char|
       counter[char] += 1
     end
     counter.to_a.max_by { |k, v| v }[0][0]
@@ -8,6 +8,6 @@ def get_most_common_letter(text)
   
   # Intended output:
   # 
-  # > get_most_common_letter("the roof, the roof, the roof is on fire!")
+  get_most_common_letter("the roof, the roof, the roof is on fire!")
   # => "o"
   
